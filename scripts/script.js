c7bac39c612123a3f5b0e8ef38ee438b74d03c21
DanielloXXI <cybiran_ez@mail.ru>
2022-11-27 23:14:36 +0300

    mesto

--- scripts/script.js
+++ scripts/script.js
@@ -4,24 +4,23 @@ let profileText = document.querySelector(".profile__text");
 let profileName = profileText.querySelector(".profile__name");
 let profileStatus = profileText.querySelector(".profile__status");
 let popupCloseButton = popup.querySelector(".popup__close-button");
-let popupName = document.querySelector(".popup__name");
-let popupDescription = popup.querySelector(".popup__description");
+let popupName = document.querySelector(".popup__input_name");
+let popupDescription = popup.querySelector(".popup__input_description");
 let popupForm = popup.querySelector(".popup__form");
-
 function showPopup() {
     popup.classList.add("popup_opened");
     popupName.value = profileName.textContent;
     popupDescription.value = profileStatus.textContent;
 }
-profileEditButton.addEventListener("click", showPopup);
 function closePopup() {
     popup.classList.remove("popup_opened");
 }
-popupCloseButton.addEventListener("click", closePopup);
 function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileStatus.textContent = popupDescription.value;
-    popup.classList.remove("popup_opened");
+    closePopup();
 }
+popupCloseButton.addEventListener("click", closePopup);
+profileEditButton.addEventListener("click", showPopup);
 popupForm.addEventListener('submit', handleFormSubmit); 
