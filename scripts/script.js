2e9f71963a7f863d9a1eec9ac6aad7ee6dc78480
DanielloXXI <cybiran_ez@mail.ru>
2022-12-08 22:32:54 +0300

    Mesto(global edit)

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ let profileEditButton = document.querySelector(".profile__edit-button");
 let profileText = document.querySelector(".profile__text");
 let profileName = profileText.querySelector(".profile__name");
 let profileStatus = profileText.querySelector(".profile__status");
-let popupCloseButton = popupEdit.querySelector(".popup__close-button");
+let popupEditCloseButton = popupEdit.querySelector(".popup__edit_close-button");
 let popupName = document.querySelector(".popup__input_name");
 let popupDescription = popupEdit.querySelector(".popup__input_description");
 let popupEditForm = popupEdit.querySelector(".popup__edit_form");
@@ -11,6 +11,10 @@ let popupAdd = document.querySelector(".popup__add");
 let popupAddButton = document.querySelector(".profile__add-button");
 let popupAddCloseButton = document.querySelector(".popup__add_close-button");
 let popupAddForm = document.querySelector(".popup__add_form");
+let elementTemplate = document.querySelector("#element").content;
+let elements = document.querySelector(".elements");
+let popupInputTitle = document.querySelector(".popup__input_title");
+let popupInputLink = document.querySelector(".popup__input_link");
 
 const initialCards = [
     {
@@ -39,7 +43,16 @@ const initialCards = [
     }
 ];
 
-
+initialCards.forEach(function (cards) {
+    const element = elementTemplate.cloneNode(true);
+    element.querySelector(".element__name").textContent = cards.name;
+    element.querySelector(".element__image").src = cards.link;
+    element.querySelector(".element__image").alt = `фото: ${cards.name}`;
+    element.querySelector(".element__like").addEventListener("click", function (evt) {
+        evt.target.classList.toggle("element__like_active");
+    })
+    elements.append(element);
+})
 
 function showPopupEdit() {
     popupEdit.classList.add("popup_opened");
@@ -66,12 +79,24 @@ function closePopupAdd() {
     popupAdd.classList.remove("popup_opened");
 }
 
+function ElementAdd() {
+    const element = elementTemplate.querySelector(".element").cloneNode(true);
+    element.querySelector(".element__name").textContent = popupInputTitle.value;
+    element.querySelector(".element__image").src = popupInputLink.value;
+    element.querySelector(".element__image").alt = `фото: ${popupInputTitle.value}`;
+    element.querySelector(".element__like").addEventListener("click", function (evt) {
+        evt.target.classList.toggle("element__like_active");
+    })
+    elements.prepend(element);
+}
+
 function handleFormAddSubmit(evt) {
     evt.preventDefault();
+    ElementAdd();
     closePopupAdd();
 }
 
-popupCloseButton.addEventListener("click", closePopupEdit);
+popupEditCloseButton.addEventListener("click", closePopupEdit);
 profileEditButton.addEventListener("click", showPopupEdit);
 popupEditForm.addEventListener('submit', handleFormEditSubmit);
 popupAddButton.addEventListener("click", showPopupAdd);
